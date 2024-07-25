a8f0b1c2bdd96ce30e5238b9ee14264f5a67ff10
zicros <zilongz@gmail.com>
2024-07-25 12:18:55 -0400

    Enabling surround ZSH feature

---
+++ .config/zsh/init/40-vi-surround.zsh
@@ -0,0 +1,11 @@
+# Enable ZSH features: VIM style surrounding
+autoload -Uz surround
+
+# ZSH Surround feature
+zle -N delete-surround surround
+zle -N add-surround surround
+zle -N change-surround surround
+bindkey -a cs change-surround
+bindkey -a ds delete-surround
+bindkey -a ys add-surround
+bindkey -M visual S add-surround
