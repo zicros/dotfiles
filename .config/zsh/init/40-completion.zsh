b449e115b1011d2fc8f92414bdd756d52f732700
Robert Zhao <zilongz@gmail.com>
2025-08-16 00:49:01 -0400

    Adding zsh completion using TAB TAB

---
+++ .config/zsh/init/40-completion.zsh
@@ -0,0 +1,3 @@
+# `menu select` will enable the TAB TAB completion menu
+# Everything else the order in which to complete.
+zstyle ':completion:*' menu select completer _complete _ignored _expand _prefix
