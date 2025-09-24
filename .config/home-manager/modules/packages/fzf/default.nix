be47f3c20547293d21fbe1119f064544cb312a00
Robert Zhao <zilongz@gmail.com>
2025-09-24 11:54:22 -0400

    Updating fzf package

--- .config/home-manager/modules/packages/fzf/default.nix
+++ .config/home-manager/modules/packages/fzf/default.nix
@@ -1,20 +1,21 @@
 { pkgs }:
 let
     bin = "fzf";
-    version = "0.60.3";
+    version = "0.65.2";
 
     uri_arch_name = if pkgs.stdenv.hostPlatform.system == "aarch64-linux" then "arm64" else "amd64";
 
     target_file_name = "fzf-${version}-linux_${uri_arch_name}.tar.gz";
 
     # Quick script to get the checksums:
-    # curl -sL https://github.com/junegunn/fzf/releases/download/v0.60.3/fzf_0.60.3_checksums.txt | \
+    # VERSION="0.65.2"
+    # curl -sL https://github.com/junegunn/fzf/releases/download/v${VERSION}/fzf_${VERSION}_checksums.txt | \
     #     grep "linux_\(amd\|arm\)64" | \
     #     awk '{print $2 " = \"" $1 "\";"}' | \
     #     sed 's/^fzf[^ ]*\(amd64\|arm64\)\.tar\.gz/\1/'
     pkg_checksums = {
-        amd64 = "2937a4f10b0f80e0c974d9459df3bc049b068a97212b0d253c36c9da5920b521";
-        arm64 = "13df4d556992938a4beb340ac3b17c51c77e46db978d3071429eb77a94c581c1";
+        amd64 = "5eb8efc0e94aa559f84ea83eeba99bea7dce818e63f92b4b62e60663220f1c14";
+        arm64 = "097347160595bf03a426d2abe0a17e14ca060540ddfc0ea45c0a9be62bb29a2b";
     };
 in
 pkgs.stdenv.mkDerivation {
